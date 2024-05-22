102d20114b8d58d8ca5bcd29c45db9e85121bd1c
anderson sfiems <kardek.anderson@gmail.com>
2024-05-22 17:07:13 -0400

    feat: sqlserver

--- PassIn.Infrastructure/PassInDbContext.cs
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -6,6 +6,9 @@ public class PassInDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=C:\\codes\\chsarp\\PassIn\\PassInDb.db");
+        //optionsBuilder.UseSqlite("Data Source=C:\\codes\\chsarp\\PassIn\\PassInDb.db");
+        optionsBuilder.UseSqlServer("Server=localhost\\1200;Database=cashFlow;Uid=sa;Pwd=SQLserver01@;Trusted_Connection=True;TrustServerCertificate=True;");
+
     }
 }
+//oasdask
